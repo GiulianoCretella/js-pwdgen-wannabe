7d72f110116a5e87caa368c35ffd70630fa2b5a0
GiulianoCretella <giulianocretella.1@gmail.com>
2022-03-23 15:01:33 +0100

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-alert('Ciao!')
+// nome ,cognome, colorepreferito,21
+
+let yourName = prompt ('Il tuo Nome?');
+console.log(yourName);
+
+let yourSurname = prompt('Il tuo Cognome?');
+console.log(yourSurname);
+
+let yourColor = prompt('Il tuo colore preferito?');
+console.log(yourColor);
+
+let password = yourName + yourSurname + yourColor + '21';
+console.log(password);
+document.getElementById('passwordgen').innerHTML += ' ' + password; 
