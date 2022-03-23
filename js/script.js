0146af34c0a7696a98d4f3d1a447952dd7b2ed47
GiulianoCretella <giulianocretella.1@gmail.com>
2022-03-23 15:38:03 +0100

    bonus flowchart

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-// nome ,cognome, colorepreferito,21
+// graph TD
+//    A[ creo variabile per nome utente tramite prompt]--> B[creo variabile per cognome utente tramite prompt]
+//    B --> C[creo variabile per colore preferito tramite prompt]
+//    C --> D[creo una variabile che concatena le 3 variabili/credenziali indicate e il numero '21']
+//    D --> E[tramite 'getElementById' importo la password generata nel html assegnandola con un operatore composto '+=']
+//    E --> Fine
+  
 
 let yourName = prompt ('Il tuo Nome?');
 console.log(yourName);
@@ -11,4 +17,5 @@ console.log(yourColor);
 
 let password = yourName + yourSurname + yourColor + '21';
 console.log(password);
+
 document.getElementById('passwordgen').innerHTML += ' ' + password; 
